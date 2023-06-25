c7989150da99c1a231c06559ecd8807b6f09c8dc
Diego Peña Vicente <diego10azul@hotmail.com>
2023-06-25 13:36:22 +0200

    specifiy variables

--- src/main/java/com/medicapp/server/MedicappApplication.java
+++ src/main/java/com/medicapp/server/MedicappApplication.java
@@ -1,5 +1,6 @@
 package com.medicapp.server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class MedicappApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedicappApplication.class, args);
     }
-
 }
