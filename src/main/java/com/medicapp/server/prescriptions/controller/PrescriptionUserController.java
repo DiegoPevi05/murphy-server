79e3263bfc1567df0453acda66e211a246a88a71
Diego <diego10azul@hotmail.com>
2023-06-28 16:52:43 +0200

    update prescriptions

--- src/main/java/com/medicapp/server/prescriptions/controller/PrescriptionUserController.java
+++ src/main/java/com/medicapp/server/prescriptions/controller/PrescriptionUserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/doctor/prescription")
+@RequestMapping("/api/v1/user/prescription")
 @RequiredArgsConstructor
 public class PrescriptionUserController {
 
